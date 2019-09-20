99a970debee90a95acf7751e903ff637d9d33334
jransome <jamesransome@msn.com>
2019-09-20 16:57:10 +0100

    Add skybox

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+  public bool FollowUpDown = true;
   public Rigidbody Ball;
   public float CameraSlerp = 0.2f;
 
   private void FixedUpdate()
   {
-    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Ball.velocity, Vector3.up), CameraSlerp);
+    Vector3 targetVector = FollowUpDown ? Ball.velocity : new Vector3(Ball.velocity.x, 0f, Ball.velocity.z);
+    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetVector, Vector3.up), CameraSlerp);
     transform.position = Ball.position;
   }
 }
